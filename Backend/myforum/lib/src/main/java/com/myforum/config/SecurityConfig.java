2fb714012fc67fa2cdbfd666ea48712df09a28ea
ParthHalwane <parthhalwane@gmail.com>
2024-12-31 12:07:50 +0530

    add github configs

--- Backend/myforum/lib/src/main/java/com/myforum/config/SecurityConfig.java
+++ Backend/myforum/lib/src/main/java/com/myforum/config/SecurityConfig.java
@@ -3,26 +3,21 @@ package com.myforum.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/auth/**").permitAll()  // Changed to antMatchers
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	http
+        .csrf().disable()
+        .authorizeRequests()
+            .antMatchers("/auth/**", "/", "/oauth2/**").permitAll()
             .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+        .and()
+        .oauth2Login()
+            .defaultSuccessUrl("/home", true);
 
         return http.build();
     }
